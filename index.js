63149efb316eb127692203acf8ec3737c07f1004
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2025-01-09 09:00:58 +0530

    add more code in js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function makeHttprequest(method, url){
+function makeHttprequest(method, url,callback){
     const xhr = new XMLHttpRequest()
     xhr.responseType = 'json';
     xhr.addEventListener('load', () => {
+        callback(xhr.response);
+        // console.log(xhr.response.users.length);
+
         
     })
+    xhr.open(method,url)
+    xhr.send()
 }
-makeHttprequest('GET','https://dummyjson.com/users')
+makeHttprequest('GET','https://dummyjson.com/users', (userdata) => {
+    makeHttprequest('GET', 'https://dummyjson.com/users/${userdata[xhr.response.length]}')
+}
+// makeHttprequest('GET','https://dummyjson.com/users')

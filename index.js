3ece73e9646d74996923db0c4f08b132afbe86ae
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2025-01-09 10:03:33 +0530

    commit

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ function makeHttprequest(method, url,callback){
     const xhr = new XMLHttpRequest()
     xhr.responseType = 'json';
     xhr.addEventListener('load', () => {
-        callback(xhr.response);
-        // console.log(xhr.response.users.length);
-
-        
+        callback(xhr.response)
     })
     xhr.open(method,url)
     xhr.send()
 }
 makeHttprequest('GET','https://dummyjson.com/users', (userdata) => {
-    makeHttprequest('GET', 'https://dummyjson.com/users/${userdata[xhr.response.length]}')
-}
+    makeHttprequest('GET',`https://dummyjson.com/users/${userdata.users[(userdata.users).length].id}`)
+    
+})
 // makeHttprequest('GET','https://dummyjson.com/users')

e41a8bcf229327ba19ecfff160aab93b0fbf4794
VishnuShreeKrishna <Vishnusharma5615@gmail.com>
2025-01-15 08:06:04 +0530

    comit

--- index.js
+++
@@ -1,6 +0,0 @@
-function makeHttprequest(method, url,callback){
-    const xhr = new XMLHttpRequest()
-    // xhr.responseType = 'json'
-    console.log(xhr.responseType)
-}
-makeHttprequest('GET','https://dummyjson.com/users')

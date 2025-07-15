308094d7d42b360b0a2be8b1c11fc8ceaef65d5f
VishnuShreeKrishna <vishnusharma5615@gmail.com>
2025-07-15 09:40:00 +0530

    commit

--- src/script.js
+++ src/script.js
@@ -1,9 +1,17 @@
 function customRender(reactElement, container) {
-    const domElement = document.createElement(reactElement.type);
+   /* const domElement = document.createElement(reactElement.type);
     domElement.setAttribute("href", reactElement.props.href);
     domElement.innerHTML = reactElement.children;
     domElement.setAttribute("target", reactElement.props.target);
 
+    container.appendChild(domElement);
+    */
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+    for(const prop in reactElement.props) {
+        if (prop === "children") continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
     container.appendChild(domElement);
 }
 const reactElement = {
@@ -15,5 +23,5 @@ const reactElement = {
         children: "Click me to Google",
 }
 
-
 const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
